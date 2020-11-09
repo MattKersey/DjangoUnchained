96c1220a94fcb49f7f55679772180ce88fc544d3
Carlos Eguiluz Rosas <ceguiluzrosas@gmail.com>
2020-11-09 14:36:00 -0500

    updating references to new auth user model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework import permissions
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -9,16 +9,6 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
 
-    queryset = User.objects.all().order_by("-date_joined")
+    queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,6 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
