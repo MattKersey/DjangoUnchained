9763694ab013c3db9c0c9ed46efa0c00d19f850d
jkim10 <jyk2149@columbia.edu>
2020-11-12 21:40:53 -0500

    Sorry jumbled pr

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Should default to login if not authenticated</p>
+      </div>
+    )
+  }
+}
+
+export default Home
