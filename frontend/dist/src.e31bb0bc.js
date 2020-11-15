9f69cb00dfe20e9f0c9f343b25f6a36a69a2c21b
Matt Kersey <mkersey95@gmail.com>
2020-11-15 08:49:19 -0500

    Fixed issue with ESLint

--- frontend/dist/src.e31bb0bc.js
+++ frontend/dist/src.e31bb0bc.js
@@ -59259,7 +59259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55466" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58335" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
