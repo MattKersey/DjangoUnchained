82ccc43a191d6265677a02ce1b58d07ca376f5a5
MattKersey <matthew.l.kersey.18@dartmouth.edu>
2020-11-19 15:52:36 -0500

    Added stubs for user endpoints

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -3,39 +3,95 @@ from rest_framework.test import APITestCase
 from api.models import User
 import datetime
 import requests_mock
-
 from oauth2_provider.models import get_application_model, get_access_token_model
-
 Application = get_application_model()
 AccessToken = get_access_token_model()
 
 
+def setupOAuth(self):
+    CLIENT_ID = "abselskfjlskdfj"
+    CLIENT_SECRET = "laksdfjlaksjdflksjdflksdjf"
+    self.user = User.objects.create_superuser(
+        email="superuserOAuth@email.com", password="superuser"
+    )
+    self.application = Application.objects.create(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        client_type=Application.CLIENT_CONFIDENTIAL,
+        name="OAuth-Test-API",
+        redirect_uris="http://127.0.0.1:8000/authredirect/",
+        skip_authorization=True
+    )
+    self.application.save()
+    self.token = AccessToken.objects.create(
+        user=self.user,
+        token="adfslkfjavsdfeslfkjgh",
+        application=self.application,
+        expires=timezone.now() + datetime.timedelta(days=1),
+        scope="read write",
+    )
+    self.token.save()
+
+
+class Test_UserVieww(APITestCase):
+    def setUp(self):
+        self.userExists = User.objects.create_user(
+            email="exists@email.com", password="password"
+        )
+        setupOAuth(self)
+
+    def test_register_good(self):
+        url = "http://127.0.0.1:8000/api/register/"
+        user = {
+            "email": "testuser@example.com",
+            "password": "testpassword"
+        }
+        r = self.client.post(url, user)
+        print(r)
+        # self.assertEqual(201, r.status_code)
+        # self.assertEqual(1, r.data["status"])
+
+    def test_register_bad(self):
+        url = "http://127.0.0.1:8000/api/register/"
+        user = {
+            "email": "exists@example.com",
+            "password": "testpassword"
+        }
+        r = self.client.post(url, user)
+        print(r)
+        # self.assertEqual(400, r.status_code)
+        # self.assertEqual(0, r.data["status"])
+
+    def test_retrieve_user(self):
+        url = "http://127.0.0.1:8000/api/users/"
+        r = self.client.get(
+            url + str(self.userExists.pk),
+            HTTP_AUTHORIZATION="Bearer " + self.token.token
+        )
+        print(r)
+
+    def test_list_user(self):
+        url = "http://127.0.0.1:8000/api/users/"
+        r = self.client.get(
+            url,
+            HTTP_AUTHORIZATION="Bearer " + self.token.token
+        )
+        print(r)
+
+    def test_add_store(self):
+        pass
+
+    def test_remove_store(self):
+        pass
+
+    def test_delete_store(self):
+        pass
+
+
 class Test_OAuth(APITestCase):
     def setUp(self):
-        # oauth2_settings._SCOPES = ["read", "write"
-        CLIENT_ID = "abselskfjlskdfj"
-        CLIENT_SECRET = "laksdfjlaksjdflksjdflksdjf"
-        self.user = User.objects.create_superuser(
-            email="superuserOAuth@email.com", password="superuser"
-        )
-        self.application = Application.objects.create(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
-            client_type=Application.CLIENT_CONFIDENTIAL,
-            name="OAuth-Test-API",
-            redirect_uris="http://127.0.0.1:8000/authredirect/",
-            skip_authorization=True
-        )
-        self.application.save()
-        self.token = AccessToken.objects.create(
-            user=self.user,
-            token="adfslkfjavsdfeslfkjgh",
-            application=self.application,
-            expires=timezone.now() + datetime.timedelta(days=1),
-            scope="read write",
-        )
-        self.token.save()
+        setupOAuth(self)
 
     def test_bad_access(self):
         url = "http://127.0.0.1:8000/api/ping/"
