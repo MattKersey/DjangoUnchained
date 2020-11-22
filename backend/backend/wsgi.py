969d2d930373dce572fa9e0efce0c83d67c05a1c
Matt Kersey <mkersey95@gmail.com>
2020-11-22 13:59:55 -0500

    Current user endpoint at /api/users/current_user

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -2,9 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
