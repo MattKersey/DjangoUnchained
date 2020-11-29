2f70b83e6467c7188399e80cf5254b26d27c9c8b
Matt Kersey <mkersey95@gmail.com>
2020-11-28 20:11:58 -0500

    Fixed a few small errors, users can now obtain read and write scopes connected to specific stores, but they do not do anything

--- backend/backend/scopes.py
+++ backend/backend/scopes.py
@@ -6,20 +6,20 @@ class CustomScopes(BaseScopes):
     def get_all_scopes(self):
         scopes = {"read": "custom reading scope", "write": "custom writing scope"}
         for store in Store.objects.all():
-            scopes["store_" + store.pk + ":read", "Reading scope for " + store.name]
-            scopes["store_" + store.pk + ":write", "Writing scope for " + store.name]
+            scopes["store_" + str(store.pk) + ":read"] = "Reading scope for " + store.name
+            scopes["store_" + str(store.pk) + ":write"] = "Writing scope for " + store.name
         return scopes
 
     def get_available_scopes(self, application=None, request=None, *args, **kwargs):
         scopes = ["read", "write"]
         for store in Store.objects.all():
-            scopes.append("store_" + store.pk + ":read")
-            scopes.append("store_" + store.pk + ":write")
+            scopes.append("store_" + str(store.pk) + ":read")
+            scopes.append("store_" + str(store.pk) + ":write")
         return scopes
 
     def get_default_scopes(self, application=None, request=None, *args, **kwargs):
         scopes = ["read", "write"]
         for store in Store.objects.all():
-            scopes.append("store_" + store.pk + ":read")
-            scopes.append("store_" + store.pk + ":write")
+            scopes.append("store_" + str(store.pk) + ":read")
+            scopes.append("store_" + str(store.pk) + ":write")
         return scopes
