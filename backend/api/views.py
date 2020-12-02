eb8dea515593d0371c95e07d39297bac0aeb4bb9
jkim10 <jyk2149@columbia.edu>
2020-12-02 16:50:33 -0500

    removing unneeded endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -422,25 +422,6 @@ class StoreViewSet(viewsets.ViewSet):
                 {"message": "The item cannot be deleted."},
                 status=status.HTTP_406_NOT_ACCEPTABLE,
             )
-    @action(detail=True, methods=["POST"])
-    def create_checkout_session():
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-            'price_data': {
-                'currency': 'usd',
-                'product_data': {
-                'name': 'T-shirt',
-                },
-                'unit_amount': 2000,
-            },
-            'quantity': 1,
-            }],
-            mode='payment',
-            success_url='https://example.com/success',
-            cancel_url='https://example.com/cancel',
-        )
-        return Response(data={'id': session.id})
 
 
 class ItemViewSet(viewsets.ViewSet):
