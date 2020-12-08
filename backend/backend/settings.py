538891092c418e7f54226b64c970345f4ad0f209
Matt Kersey <mkersey95@gmail.com>
2020-12-08 12:50:40 -0500

    Fixed placement of load_dotenv, now tests and running the app will work locally

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -2,10 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
